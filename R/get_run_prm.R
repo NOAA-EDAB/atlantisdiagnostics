1ea134451b7c14c5de4864a225807a61dcbb24fa
andybeet <22455149+andybeet@users.noreply.github.com>
2025-04-17 17:55:12 -0400

    add trailing white space to grep to prevent multiple parameter matches

--- R/get_run_prm.R
+++ R/get_run_prm.R
@@ -1,7 +1,9 @@
 #' Get parameters from the run prm file
 #'
 #' @description
-#' Pull out the parameters from the run prm file.
+#' Pull out the parameters from the run prm file. This will only return the values of
+#' parameters that have a scalar value. For example, if paramName = init_scalar, only the length
+#' of the vector will be returned.
 #'
 #'
 #' @param runprm path to the run prm file
@@ -22,13 +24,13 @@ get_run_prm <- function(runprm, paramName = NULL ){
   # read n parameter file
   lines <-  readLines(runprm)
   # identify lines that contain the paramName
-  lineNum <-  grep(paste0("^",paramName),lines)
+  lineNum <-  grep(paste0("^",paramName," "),lines)
   # extract the lines
   line <- lines[lineNum]
-  value <- unlist(strsplit(line,"#"))[1]
-  value <- gsub(paramName,"",value)
-  value <- trimws(value)
-  final <- unlist(strsplit(value,"\\s+"))
+  linestr <- unlist(strsplit(line,"#"))[1]
+  linestr <- gsub(paramName,"",linestr)
+  linestr <- trimws(linestr)
+  final <- unlist(strsplit(linestr,"\\s+"))
   out <- list()
 
   # grepl if any letters of characters
